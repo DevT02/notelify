564abb013379e0d8673a5f6a3afd7aeb98db9b0c
DevT02 <devliveforever@gmail.com>
2023-11-06 23:09:27 -0500

    default params

--- optimized_prompt/main.py
+++ optimized_prompt/main.py
@@ -2,10 +2,17 @@
 
 import openai
 import argparse
-from config import OPENAI_API_KEY
-# Must Define key
+from config import OPENAI_API_KEY # define key in config.py
+import time
+import os
 
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+default_input_file = os.path.join(script_directory, "input.txt")  
+
+chars_before_update = 50 # Number of characters before updating the prompt
 input_prompt = "Are you able to convert text into transcription that fits a markdown style, as in a traditional page on a note-taking website or github readme, which splits words and text into headers, bulletpoints, bolding, italics, underlines, etc (and any combination). ensure YOU DO NOT deviate from this style format for every message given to you. Sometimes, the message will not be long enough, and you may need to wait a bit before processing the file. DO NOT just convert text to markdown. Highlight important bits using a traditional style guide and create it as if you were writing notes for a class. "
+
 def convert_to_markdown(text, response_length = "medium"):
     prompt = f"{input_prompt}:\n\n{text}\n\n---\n\n"
 
@@ -23,27 +30,35 @@ def convert_to_markdown(text, response_length = "medium"):
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=response_length  # Adjust this as needed to control the response length
+        max_tokens=response_length 
     )
 
     return response.choices[0].text.strip()
 
 def main():
     parser = argparse.ArgumentParser(description="Convert text to Markdown style using GPT")
-    parser.add_argument("input_file", help="Path to the input text file")
+    parser.add_argument("input_file", nargs="?", default=default_input_file, help="Path to the input text file")
     parser.add_argument("--response_length", choices=["summary", "regular", "long"], default="regular", 
                         help="Response length (summary, regular, long) [default: regular]")
 
     args = parser.parse_args()
 
-    with open(args.input_file, 'r') as file:
-        text = file.read()
+    last_length = 0
 
-    while text:
-        markdown_text = convert_to_markdown(text, args.response_length)
-        print(markdown_text)
-
-        text = file.read()
+    while True:
+        with open(args.input_file, 'r') as file:
+            text = file.read()
+            if len(text) > last_length + chars_before_update:
+                #since text has grown, lets update the text.
+                markdown_text = convert_to_markdown(text, args.response_length)
+                print(markdown_text)
+                last_length = len(text)
+            
+            if len(text) < last_length:
+                # The text got shorter, reset the last_length
+                last_length = len(text)
+        time.sleep(15)    
+        #await asyncio.sleep(30) # await for 30 seconds (no given task, may be used for other tasks)
 
 if __name__ == "__main__":
     main()
